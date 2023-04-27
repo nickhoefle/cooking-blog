3b8cce1449d08f5f0c4e0e40deaf94cc33d65b9c
Nick <nickhoefle@gmail.com>
2023-04-27 17:23:35 -0500

    Homepage complete! Navbar, posts, recent posts sidebar and categories sidebar working.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ posts }) {
         </Head>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
           <div className="lg:col-span-8 col-span-1">
-            {posts.map((post, index) => <PostCard post={post} key={post.title} />)}
+            {posts.map((post, index) => <PostCard post={post.node} key={post.title} />)}
           </div>
           <div className="lg:col-span-4 col-4-span-1">
             <div className="lg:sticky relative top-8">
